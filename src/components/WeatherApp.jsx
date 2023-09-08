f00c6af3efbc08c5d15283904622e33aa697a30a
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2023-09-08 15:16:37 +0100

    the bottom weather updates

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect,useRef, useState } from "react";
 import location from "../assets/location.png";
 import search from "../assets/search-interface-symbol.png";
 import weather from "../assets/cloudy.png";
@@ -12,10 +12,23 @@ import wind from "../assets/wind.png";
 import "./WeatherApp.css";
 const WeatherApp = () => {
     const [temperature1, setTemperature] = useState("22°C")
+    const cities = ["Lagos","New-york","London"]
     const [humidity1, setHumidity] = useState("78%")
     const [wind1, setWind] = useState("6 km/h")
     const [location1, setLocation] = useState("London")
     const [weatherIcon,setWeatherIcon] = useState(weather)
+    const [lagaosTemp, setLagosTemp] = useState("22°C")
+    const [lagosLocation, setLagosLocation] = useState("lagos")
+    const [lagosHumidity, setLagosHumidity] = useState("78%")
+    const [lagosWind, setLagosWind] = useState("6km/h")
+    const [londonTemp, setLondonTemp] = useState("22°C")
+    const [londonLocation, setLondonLocation] = useState("london")
+    const [londonHumidity, setLondonHumidity] = useState("78%")
+    const [londonWind, setLondonWind] = useState("6km/h")
+    const [NewyorkTemp, setNewyorkTemp] = useState("22°C")
+    const [NewyorkLocation, setNewyorkLocation] = useState("Newyork")
+    const [NewyorkHumidity, setNewyorkHumidity] = useState("78%")
+    const [NewyorkWind, setNewyorkWind] = useState("6km/h")
     let api_key = "a12806d321d64d33bc1211042230609"
     const inputRef = useRef(null)
     const handleSubmit = async () => {
@@ -57,6 +70,48 @@ const WeatherApp = () => {
         setWeatherIcon(weather);
       }
     }
+
+    useEffect(() => {
+      async function fetchCountries() {
+        try {
+          for (const city of cities) {
+            let url2 = `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=no`;
+            const res = await fetch(url2);
+            const datalog = await res.json();
+            const wholeNumberTemperature = Math.trunc(datalog.current.temp_c);
+            const wholeNumberWind = Math.trunc(datalog.current.wind_kph)
+            switch (city) {
+              case "Lagos":
+                setLagosTemp(`${wholeNumberTemperature}`);
+                setLagosLocation(datalog.location.name);
+                setLagosHumidity(datalog.current.humidity)
+                setLagosWind(wholeNumberWind)
+                break;
+              case "London":
+                setLondonTemp(`${wholeNumberTemperature}`);
+                setLondonLocation(datalog.location.name);
+                setLondonHumidity(datalog.current.humidity)
+                setLondonWind(wholeNumberWind)
+                break;
+              case "New-york":
+                setNewyorkTemp(`${wholeNumberTemperature}`);
+                setNewyorkLocation(datalog.location.name);
+                setNewyorkHumidity(datalog.current.humidity)
+                setNewyorkWind(wholeNumberWind)
+                break;
+              default:
+                break;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+      fetchCountries();
+    }, []);
+    
+    
   return (
     <div className="weather-app">
       <div className="date">Today,6 September</div>
@@ -103,21 +158,21 @@ const WeatherApp = () => {
             <img src={weather} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
-              <div className="city-text2">London</div>
+              <div className="city-text2">{lagosLocation}</div>
             </div>
             </div>
             <div className="city-weather-info">
               <div className="city-wind">
               <div className="city-text3">wind</div>
-              <div className="city-text4">352</div>
+              <div className="city-text4">{lagosWind}<br></br>km/h</div>
               </div>
               <div className="city-temperatue">
               <div className="city-text5">temperature</div>
-              <div className="city-text6">18°c</div>
+              <div className="city-text6">{lagaosTemp + "°C"}</div>
               </div>
               <div className="city-humidity">
               <div className="city-text7">humidity</div>
-              <div className="city-text8">25%</div>
+              <div className="city-text8">{lagosHumidity + "%"}</div>
               </div>
             </div>
           </div>
@@ -126,21 +181,21 @@ const WeatherApp = () => {
             <img src={weather} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
-              <div className="city-text2">London</div>
+              <div className="city-text2">{londonLocation}</div>
             </div>
             </div>
             <div className="city-weather-info">
               <div className="city-wind">
               <div className="city-text3">wind</div>
-              <div className="city-text4">352</div>
+              <div className="city-text4">{londonWind}<br></br>km/h</div>
               </div>
               <div className="city-temperatue">
               <div className="city-text5">temperature</div>
-              <div className="city-text6">18°c</div>
+              <div className="city-text6">{londonTemp + "°C"}</div>
               </div>
               <div className="city-humidity">
               <div className="city-text7">humidity</div>
-              <div className="city-text8">25%</div>
+              <div className="city-text8">{londonHumidity + "%"}</div>
               </div>
             </div>
           </div>
@@ -149,21 +204,21 @@ const WeatherApp = () => {
             <img src={weather} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
-              <div className="city-text2">London</div>
+              <div className="city-text2">{NewyorkLocation}</div>
             </div>
             </div>
             <div className="city-weather-info">
               <div className="city-wind">
               <div className="city-text3">wind</div>
-              <div className="city-text4">352</div>
+              <div className="city-text4">{NewyorkWind}<br></br>km/h</div>
               </div>
               <div className="city-temperatue">
               <div className="city-text5">temperature</div>
-              <div className="city-text6">18°c</div>
+              <div className="city-text6">{NewyorkTemp + "°C"}</div>
               </div>
               <div className="city-humidity">
               <div className="city-text7">humidity</div>
-              <div className="city-text8">25%</div>
+              <div className="city-text8">{NewyorkHumidity + "%"}</div>
               </div>
             </div>
           </div>
