031c7cdc4ec03aac79fbfc985c44a55f8cc22468
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2023-09-09 21:55:41 +0100

    updated changes

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -2,11 +2,6 @@ import React, { useEffect,useRef, useState } from "react";
 import location from "../assets/location.png";
 import search from "../assets/search-interface-symbol.png";
 import weather from "../assets/cloudy.png";
-import Sunny from "../assets/sun.png"
-import drizzling from "../assets/drizzling.png"
-import heavyRain from "../assets/heavy-rain.png"
-import snowy from "../assets/snowy.png"
-import thunderStorm from "../assets/thunderstorm.png"
 import humidity from "../assets/humidity.png";
 import wind from "../assets/wind.png";
 import "./WeatherApp.css";
@@ -17,18 +12,23 @@ const WeatherApp = () => {
     const [wind1, setWind] = useState("6 km/h")
     const [location1, setLocation] = useState("London")
     const [weatherIcon,setWeatherIcon] = useState(weather)
-    const [lagaosTemp, setLagosTemp] = useState("22°C")
-    const [lagosLocation, setLagosLocation] = useState("lagos")
-    const [lagosHumidity, setLagosHumidity] = useState("78%")
-    const [lagosWind, setLagosWind] = useState("6km/h")
-    const [londonTemp, setLondonTemp] = useState("22°C")
+    const [peripheralWeatherIcon,setPeripheralWeatherIcon] = useState(weather)
+    const [lagaosTemp, setLagosTemp] = useState("22")
+    const [lagosLocation, setLagosLocation] = useState("london")
+    const [lagosHumidity, setLagosHumidity] = useState("78")
+    const [lagosWind, setLagosWind] = useState("6")
+    const [londonTemp, setLondonTemp] = useState("22")
     const [londonLocation, setLondonLocation] = useState("london")
-    const [londonHumidity, setLondonHumidity] = useState("78%")
-    const [londonWind, setLondonWind] = useState("6km/h")
-    const [NewyorkTemp, setNewyorkTemp] = useState("22°C")
-    const [NewyorkLocation, setNewyorkLocation] = useState("Newyork")
-    const [NewyorkHumidity, setNewyorkHumidity] = useState("78%")
-    const [NewyorkWind, setNewyorkWind] = useState("6km/h")
+    const [londonHumidity, setLondonHumidity] = useState("78")
+    const [londonWind, setLondonWind] = useState("6")
+    const [NewyorkTemp, setNewyorkTemp] = useState("22")
+    const [NewyorkLocation, setNewyorkLocation] = useState("london")
+    const [NewyorkHumidity, setNewyorkHumidity] = useState("78")
+    const [NewyorkWind, setNewyorkWind] = useState("6")
+    const currentDate = new Date()
+    const monthOfTheYear = ["january", "february", "march", "april", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+    
     let api_key = "a12806d321d64d33bc1211042230609"
     const inputRef = useRef(null)
     const handleSubmit = async () => {
@@ -47,28 +47,9 @@ const WeatherApp = () => {
       setHumidity(data.current.humidity+ "%")
       setWind(windWholeNumber+ "km/h")
       setLocation(data.location.name)
+      setWeatherIcon(data.current.condition.icon);
       console.log(data.current.condition.code)
-      if(data.current.condition.code=== "1000"){
-        setWeatherIcon(Sunny);
-      }
-      else if(data.current.condition.code=== "1240" || data.current.condition.code=== "1003"){
-        setWeatherIcon(weather)
-      }
-      else if(data.current.condition.code=== "1063"){
-        setWeatherIcon(drizzling)
-      }
-      else if(data.current.condition.code=== "1195"){
-        setWeatherIcon(heavyRain)
-      }
-      else if(data.current.condition.code=== "1066"){
-        setWeatherIcon(snowy)
-      }
-      else if(data.current.condition.code=== "1087"){
-        setWeatherIcon(thunderStorm)
-      }
-      else{
-        setWeatherIcon(weather);
-      }
+      
     }
 
     useEffect(() => {
@@ -80,28 +61,33 @@ const WeatherApp = () => {
             const datalog = await res.json();
             const wholeNumberTemperature = Math.trunc(datalog.current.temp_c);
             const wholeNumberWind = Math.trunc(datalog.current.wind_kph)
+           
             switch (city) {
               case "Lagos":
                 setLagosTemp(`${wholeNumberTemperature}`);
                 setLagosLocation(datalog.location.name);
                 setLagosHumidity(datalog.current.humidity)
                 setLagosWind(wholeNumberWind)
+                setPeripheralWeatherIcon(datalog.current.condition.icon)
                 break;
               case "London":
                 setLondonTemp(`${wholeNumberTemperature}`);
                 setLondonLocation(datalog.location.name);
                 setLondonHumidity(datalog.current.humidity)
                 setLondonWind(wholeNumberWind)
+                setPeripheralWeatherIcon(datalog.current.condition.icon)
                 break;
               case "New-york":
                 setNewyorkTemp(`${wholeNumberTemperature}`);
                 setNewyorkLocation(datalog.location.name);
                 setNewyorkHumidity(datalog.current.humidity)
                 setNewyorkWind(wholeNumberWind)
+                setPeripheralWeatherIcon(datalog.current.condition.icon)
                 break;
               default:
                 break;
             }
+            
           }
         } catch (error) {
           console.log(error);
@@ -110,11 +96,9 @@ const WeatherApp = () => {
     
       fetchCountries();
     }, []);
-    
-    
   return (
     <div className="weather-app">
-      <div className="date">Today,6 September</div>
+      <div className="date">Today,{currentDate.getDate()} {monthOfTheYear[currentDate.getMonth()]} {currentDate.getFullYear()}</div>
       <div className="search-input">
         <div className="input-box">
           <img src={location} alt="" className="location-icon" />
@@ -155,7 +139,7 @@ const WeatherApp = () => {
         <div className="city-box">
           <div className="city1">
             <div className="first-info">
-            <img src={weather} alt="" className="city-weather"/>
+            <img src={peripheralWeatherIcon} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
               <div className="city-text2">{lagosLocation}</div>
@@ -178,7 +162,7 @@ const WeatherApp = () => {
           </div>
           <div className="city1">
             <div className="first-info">
-            <img src={weather} alt="" className="city-weather"/>
+            <img src={peripheralWeatherIcon} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
               <div className="city-text2">{londonLocation}</div>
@@ -201,7 +185,7 @@ const WeatherApp = () => {
           </div>
           <div className="city1">
             <div className="first-info">
-            <img src={weather} alt="" className="city-weather"/>
+            <img src={peripheralWeatherIcon} alt="" className="city-weather"/>
             <div className="city-location">
               <div className="city-text1">location</div>
               <div className="city-text2">{NewyorkLocation}</div>
